5c1646e73c6699f72316bd7e8ad8683d113f36d6
DLulic <dorianluli1@gmail.com>
2025-09-24 11:04:28 +0200

    Instalacija Pythona kod pokretanja

--- Start.bat
+++ Start.bat
@@ -11,8 +11,9 @@ cd /d "%~dp0"
 
 echo Updating python modules...
 
+start /b /wait cmd.exe /c "winget install --id "Python.Python.3.13" --exact --source winget --accept-source-agreements --disable-interactivity --silent  --accept-package-agreements --force "
+
 start /b /wait cmd.exe /c "pip install ttkbootstrap"
 start /b /wait cmd.exe /c "pip install mysql.connector"
-start /b /wait cmd.exe /c "winget install --id "Python.Python.3.13" --exact --source winget --accept-source-agreements --disable-interactivity --silent  --accept-package-agreements --force "
 
 start /b /wait cmd.exe /c "python ./main.py"
